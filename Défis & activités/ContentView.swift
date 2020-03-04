a2fae0d00b052129f41ebc7b400cb8cbc08554e3
Mikail <mikailyavuz751900@gmail.com>
2020-03-04 11:02:33 +0100

    Initial Commit

---
+++ Défis & activités/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Défis & activités
+//
+//  Created by Mikail Yavuz on 04/03/2020.
+//  Copyright © 2020 Mikail Yavuz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
